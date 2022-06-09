34caded118be4c606a73a9a7e9f65eddfc7b6110
Manuel Lerchner <manuel.lerchner1111@gmail.com>
2022-06-09 16:51:00 +0200

    show username when logged in

--- angular-ui/src/app/mockdata/Comments.ts
+++ angular-ui/src/app/mockdata/Comments.ts
@@ -7,7 +7,7 @@ export const COMMENTS: Comment[] = [
     text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
     rating: 3,
     postedBy: {
-      name: 'Manuel',
+      username: 'Manuel',
       email: 'test@gmail.com',
       authToken: '1',
     },
@@ -18,7 +18,7 @@ export const COMMENTS: Comment[] = [
     text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
     rating: 5,
     postedBy: {
-      name: 'Manuwetwetweel',
+      username: 'Manuwetwetweel',
       email: 'test@wetewtgmail.com',
       authToken: '2',
     },
