1a305a7e4ab62220b37b95f9b9e48b1bcd6ae163
NicoSchlo <nico.schlosser99@web.de>
2022-06-12 20:28:37 +0200

    added TimeSlot, DateTimeSlow, WeekTimeSlot

--- src/main/java/application/model/util/TimeSlot.java
+++ src/main/java/application/model/util/TimeSlot.java
@@ -1,8 +1,45 @@
 package application.model.util;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.Entity;
+import java.time.LocalTime;
 
-@Embeddable
+//@Embeddable
 public class TimeSlot {
+//    @Column(name = "start_time")
+    private LocalTime startTime;
+
+//    @Column(name = "end_time")
+    private LocalTime endTime;
+
+    public TimeSlot(LocalTime startTime, LocalTime endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeSlot{" +
+                "startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
+    }
+
 }
--- src/main/java/application/model/util/WeekTimeSlot.java
+++ src/main/java/application/model/util/WeekTimeSlot.java
@@ -1,8 +1,44 @@
 package application.model.util;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 
-@Embeddable
+//@Embeddable
 public class WeekTimeSlot extends TimeSlot {
+
+//    @Column(name = "dayOfWeek")
+    private DayOfWeek dayOfWeek;
+    public WeekTimeSlot() {
+        super(LocalTime.of(12, 0), LocalTime.of(23, 59));
+    }
+
+    public WeekTimeSlot(LocalTime startTime, LocalTime endTime, DayOfWeek dayOfWeek) {
+        super(startTime, endTime);
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    @Override
+    public String toString() {
+        return "WeekTimeSlot{" +
+                "startTime=" + getStartTime() +
+                ", endTime=" + getEndTime() +
+                ", dayOfWeek=" + dayOfWeek +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        WeekTimeSlot weekTimeSlot = new WeekTimeSlot(LocalTime.of(18, 15), LocalTime.of(22,30), DayOfWeek.MONDAY);
+        System.out.println(weekTimeSlot);
+    }
+
 }
