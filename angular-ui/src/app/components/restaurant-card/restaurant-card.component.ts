e7520e81b2918424ab5a5e7e8ca9377dba3b54f2
Julian <julianjesacher@gmail.com>
2022-06-24 11:58:11 +0200

    Add function to retreive weekday form index

--- angular-ui/src/app/components/restaurant-card/restaurant-card.component.ts
+++ angular-ui/src/app/components/restaurant-card/restaurant-card.component.ts
@@ -72,4 +72,17 @@ export class RestaurantCardComponent implements OnInit {
   close() {
     this.mapService.selectedRestaurant$.next(null);
   }
+
+  getWeekDay(index: number): string{
+    switch(index){
+      case 0: return "MON";
+      case 1: return "TUE";
+      case 2: return "WED";
+      case 3: return "THU";
+      case 4: return "FRI";
+      case 5: return "SAT";
+      case 6: return "SUN";
+      default: return "";
+    }
+  }
 }
