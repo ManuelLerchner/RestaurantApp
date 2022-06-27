4b65eb5f3638866202c67968894edf5a3df020e3
NicoSchlo <nico.schlosser99@web.de>
2022-06-27 20:49:51 +0200

    removed unnecessary comments

--- src/main/java/application/model/enums/PriceCategory.java
+++ src/main/java/application/model/enums/PriceCategory.java
@@ -8,16 +8,4 @@ import java.util.Random;
 public enum PriceCategory {
     DEFAULT, CHEAP, NORMAL, COSTLY;
 
-//    private static final List<PriceCategory> VALUES =
-//            Collections.unmodifiableList(Arrays.asList(values()));
-//    private static final int SIZE = VALUES.size();
-//    private static final Random RANDOM = new Random();
-//
-//    public static PriceCategory randomPriceCategory()  {
-//        int i = RANDOM.nextInt(SIZE);
-//        if (i == 0){
-//            i=2;
-//        }
-//        return VALUES.get(i);
-//    }
 }
