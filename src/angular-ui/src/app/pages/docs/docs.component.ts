2368c39001efd1be278dd6e482a954f4f98e3486
ManuelLerchner <manuel.lerchner1111@gmail.com>
2022-07-22 23:30:28 +0200

    Revert "add documentation site"
    This reverts commit 5261d16df9171b6bec711728cd466c2873a9b112.

--- src/angular-ui/src/app/pages/docs/docs.component.ts
+++ src/angular-ui/src/app/pages/docs/docs.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-declare const SwaggerUIBundle: any;
 
 @Component({
   selector: 'app-docs',
@@ -11,17 +10,6 @@ export class DocsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const ui = SwaggerUIBundle({
-      dom_id: '#swagger-ui',
-      layout: 'BaseLayout',
-      presets: [
-        SwaggerUIBundle.presets.apis,
-        SwaggerUIBundle.SwaggerUIStandalonePreset
-      ],
-      url: 'https://api.manuellerchner.de/static/restaurantapplication-docs.json',
-      docExpansion: 'none',
-      operationsSorter: 'alpha'
-    });
   }
 
 }
